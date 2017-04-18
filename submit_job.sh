1ba9528cf889987bed8195ec99a8000efd8368b7
Chang Liu <cliu712@build-sb.(none)>
2017-04-18 16:29:11 +1200

    New CUDA version

--- submit_job.sh
+++ submit_job.sh
@@ -9,15 +9,15 @@ echo "#!/bin/bash
 #SBATCH -A uoa00436
 #SBATCH --time=05:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:1
 #SBATCH -C kepler
 #SBATCH -D $DIR
 #SBATCH --mail-user=cliu712@aucklanduni.ac.nz
 #SBATCH --mail-type=ALL
 
-module load GCC/4.9.2
-module load CUDA/7.5.18
+module load GCC/5.4.0
+module load CUDA/8.0.61
 #srun /bin/bash $DIR/build.sh
 srun $DIR/pspectre @params.txt
 #srun $DIR/pspectre
